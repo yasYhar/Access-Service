5dd275e11c086d62cd9c67e61bd87265d84154c7
yasYhar <132951296+yasYhar@users.noreply.github.com>
2025-08-31 16:52:03 +0330

    Add files via upload

---
+++ EndpointReflector.cs
@@ -0,0 +1,52 @@
+﻿using CleanArc.Application.Contracts.Persistence;
+using CleanArc.Application.Models.Dto.Admin.Controller;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArc.Web.Api.Controllers
+{
+    public class EndpointReflector : IEndpointReflector
+    {
+        private readonly Assembly _assembly;
+
+        public EndpointReflector()
+        {
+            _assembly = Assembly.GetExecutingAssembly();
+        }
+        public List<GetControllersResponseDto> GetEndpointsByArea(string Area)
+        {
+            var endpoints = new List<GetControllersResponseDto>();
+
+            var controllers = _assembly.GetTypes().Where(type => typeof(ControllerBase).IsAssignableFrom(type)
+                && !type.IsAbstract && type.GetCustomAttribute<AreaAttribute>()?.RouteValue == $"{Area}");
+
+            foreach (var controller in controllers)
+            {
+                foreach (var method in controller.GetMethods())
+                {
+                    var hasHttpAttribute = method
+                    .GetCustomAttributes(typeof(HttpMethodAttribute), inherit: true)
+                    .Any();
+
+                    if (!hasHttpAttribute)
+                        continue;
+
+                    endpoints.Add(new GetControllersResponseDto
+                    {
+                        ControllerName = controller.Name,
+                        ActionName = method.Name,
+                        Area = Area,
+                    });
+                }
+            }
+
+            return endpoints;
+        }
+    }
+}
---
+++ PermissionManager/PermissionManagerController.cs
@@ -0,0 +1,71 @@
+﻿using Asp.Versioning;
+using CleanArc.Application.Features.PermissionManager.Command;
+using CleanArc.Application.Features.PermissionManager.Query;
+using CleanArc.Application.Models.Dto.Admin.Controller;
+using CleanArc.WebFramework.BaseController;
+using Mediator;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArc.Web.Api.Controllers.Areas.Admin.PermissionManager
+{
+    [Authorize(Roles = "admin")]
+    [Area("admin")]
+    [ApiVersion("1")]
+    [ApiController]
+    [Route("api/v{version:apiVersion}/[area]/PermissionManager")]
+    public class PermissionManagerController(ISender sender) : BaseController
+    {
+        /// <summary>
+        /// دریافت اندپوینت ها
+        /// </summary>
+        /// <returns></returns> 
+        [HttpGet("GetControllers")]
+        public async Task<List<EndpointsResponseDto>?> GetControllersAync()
+        {
+            var response = new GetControllersQuery("admin");
+
+            return await sender.Send(response);
+
+        }
+
+        /// <summary>
+        /// تعریف دسترسی
+        /// </summary>
+        /// <param name="Command"></param>
+        /// <returns></returns>
+        [HttpPost("SetPermissions")]
+        public async Task<IActionResult> SetPermissionForEndpoints([FromBody]SetPermissionForControllerCommand Command) 
+        {
+            await sender.Send(Command);
+
+            return Ok("دسترسی با موفقیت اضافه شد");
+        }
+
+        /// <summary>
+        /// دریافت دسترسی ها
+        /// </summary>
+        /// <param name="Area"></param>
+        /// <returns></returns>
+        [HttpGet("GetPermissions")]
+        public async Task<List<GetPermissionsResponseDto>> GetPermissions(string? Area) 
+        {
+            var query = new GetPermissionsQuery(Area);
+
+            return await sender.Send(query);
+
+        }
+
+        /// <summary>
+        /// ایجاد دسترسی برای کاربران
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost("SetAccessForUsers")]
+        public async Task<IActionResult> SetAccess(SetAccessForUsersCommand command) 
+        {
+            await sender.Send(command);
+
+            return Ok("دسترسی با موفقیت به کاربر تخصیص شد");
+        }
+    }
+}
